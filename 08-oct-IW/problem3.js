88c9b8cb2c16ec16ecfce227fbacb1f859c349dd
Anuj190 <anujdeshmukh990@gmail.com>
2022-10-10 23:51:19 +0530

    conditional statement

---
+++ 08-oct-IW/problem3.js
@@ -0,0 +1,8 @@
+let a=20;
+let b=20;
+if (a>b){
+  console.log("Greater");
+} else {
+   console.log("Both are equal");
+}
+ 
---
+++ 08-oct-IW/problem4.js
@@ -0,0 +1,15 @@
+let user_name="abc@gmail.com";
+let pass_word="123abc";
+
+
+let user="abc@gmail.com";
+let pass="13abc";
+if (user_name==user){
+  if(pass_word==pass){
+    console.log("login successful");
+  } else{
+    console.log("incorrect password");
+  }
+} else {
+  console.log("wrong credential");
+}
